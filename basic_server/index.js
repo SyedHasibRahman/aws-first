198eb4ea24262c7040814d3572407a4dd452c8b4
SyedHasibRahman <syedhasib01@gmail.com>
2023-12-14 13:18:17 +0600

    Change data

--- basic_server/index.js
+++ basic_server/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World from aws!')
+    res.send('Hello World from aws & port removed by nginx!')
 })
 
 app.listen(port, () => {
