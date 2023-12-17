822b8288e2cfdfcdbee7aae62bb48cedd7fee9c6
SyedHasibRahman <syedhasib01@gmail.com>
2023-12-17 16:36:38 +0600

    change code

--- basic_server/index.js
+++ basic_server/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Hello World from aws & port removed by nginx!')
+    res.send('Hello World from aws & port removed by nginx  work on mobile!')
 })
 
 app.listen(port, () => {
